0cd36900d32849dca145022130cf492d32504675
thanik <thanik@gmail.com>
2020-04-28 19:52:59 +0100

    Move Editor script

--- Assets/Scripts/RailDrawInEditor.cs
+++ Assets/Scripts/RailDrawInEditor.cs
@@ -1,37 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-
-public class ReadOnlyAttribute : PropertyAttribute
-{
-
-}
-
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property,
-                                            GUIContent label)
-    {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
-
-    public override void OnGUI(Rect position,
-                               SerializedProperty property,
-                               GUIContent label)
-    {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
-}
 
 [ExecuteInEditMode]
 public class RailDrawInEditor : MonoBehaviour
 {
     public bool ShowInEditor;
-    [ReadOnly]
     public float speed;
     private Rail thisRail;
     // Start is called before the first frame update
